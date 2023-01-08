bb71e2746c479124723b8c585fc8187ee6a83cd4
Valeria Franciscangeli <vvfranciscangeli@gmail.com>
2023-01-08 20:51:17 -0300

    fin clase 112

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -3,12 +3,43 @@ import 'package:flutter/material.dart';
 class DetailsScreen extends StatelessWidget {
   const DetailsScreen({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+
+    return const Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          // widgets con comportamiento pre programado cuando se hace scroll en el padre
+          _CustomAppBar(),
+        ],
+      ),
+    );
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  const _CustomAppBar();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: const Center(child: Text('Details Screen')),
+    return SliverAppBar(
+      backgroundColor: Colors.amber,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          color: Colors.black12,
+          child: Text('movie.title', style:  TextStyle(fontSize: 16),)),
+        background: FadeInImage(placeholder: AssetImage('assets/loading.gif'),
+        image: NetworkImage('https://via.placeholder.com/500x300'),
+        fit: BoxFit.cover,),
       ),
     );
   }
