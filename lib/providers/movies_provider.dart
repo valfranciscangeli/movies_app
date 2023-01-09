603d6ad4707ddbe7ab3645c33187ee1f12509efb
Valeria Franciscangeli <vvfranciscangeli@gmail.com>
2023-01-09 04:36:24 -0300

    fin clase 129

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
 import 'package:movies_app/models/models.dart';
+import 'package:movies_app/search/search_delegate.dart';
 
 class MoviesProvider extends ChangeNotifier {
   final String _apikey = 'f5f4c74cac0a87d200eca1933ab7dcf0';
@@ -20,7 +21,7 @@ class MoviesProvider extends ChangeNotifier {
   }
 
   Future<String> _getJsonData(String endpoint, [int page = 1]) async {
-    var url = Uri.https(_baseUrl, endpoint,
+    final url = Uri.https(_baseUrl, endpoint,
         {'api_key': _apikey, 'language': _language, 'page': page.toString()});
 
     final response = await http.get(url);
@@ -47,12 +48,22 @@ class MoviesProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-  Future<List<Cast>> getMovieCast(int movieId) async { // async convierte cualquier retorno en un Future
+  Future<List<Cast>> getMovieCast(int movieId) async {
+    // async convierte cualquier retorno en un Future
     if (moviesCast.containsKey(movieId)) return moviesCast[movieId]!;
 
     final jsonData = await _getJsonData('3/movie/$movieId/credits');
     final creditsResponse = CreditsResponse.fromJson(jsonData);
     moviesCast[movieId] = creditsResponse.cast;
     return creditsResponse.cast;
+  }
+
+  Future<List<Movie>> searchMovie(String query) async {
+    final url = Uri.https(_baseUrl, '3/search/movie',
+        {'api_key': _apikey, 'language': _language, 'query': query});
+
+    final response = await http.get(url);
+    final searchResponse = SearchMovieResponse.fromJson(response.body);
+    return searchResponse.results;
   }
 }
--- lib/search/search_delegate.dart
+++ lib/search/search_delegate.dart
@@ -7,21 +7,39 @@ class MovieSearchDelegate extends SearchDelegate {
 
   @override
   List<Widget>? buildActions(BuildContext context) {
-    return [Text('builactions')];
+    return [
+      IconButton(
+          onPressed: () {
+            query = '';
+          },
+          icon: const Icon(Icons.clear))
+    ];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
-    return Text('build leading');
+    return IconButton(
+        onPressed: () {
+          close(context, null);
+        },
+        icon: Icon(Icons.arrow_back));
   }
 
   @override
   Widget buildResults(BuildContext context) {
-    return Text('build results');
+    return const Text('build results');
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
+    if (query.isEmpty) {
+      return Container(
+        child: Center(
+          child: Icon(Icons.movie_creation_outlined,
+              color: Colors.black38, size: 130),
+        ),
+      );
+    }
     return Text('build suggestions: $query');
   }
 }
